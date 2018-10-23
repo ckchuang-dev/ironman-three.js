a7aeb14aa4468bba2c70e81442e955218ce5f7a4
DezChuang <dissaivent@gmail.com>
2018-10-23 22:26:57 +0800

    [Day08] Add creeper texture code

---
+++ day08_creeperTexture/index.js
@@ -0,0 +1,156 @@
+let renderer, scene, camera
+let cameraControl, stats
+let creeperObj
+
+// 苦力怕物件
+class Creeper {
+  constructor() {
+    // 宣告頭、身體、腳幾何體大小
+    const headGeo = new THREE.BoxGeometry(4, 4, 4)
+    const bodyGeo = new THREE.BoxGeometry(4, 8, 2)
+    const footGeo = new THREE.BoxGeometry(2, 3, 2)
+
+    // 苦力怕貼圖
+    const headMap = new THREE.TextureLoader().load(
+      'https://dl.dropboxusercontent.com/s/bkqu0tty04epc46/creeper_face.png'
+    )
+    const skinMap = new THREE.TextureLoader().load(
+      'https://dl.dropboxusercontent.com/s/eev6wxdxfmukkt8/creeper_skin.png'
+    )
+
+    //
+    const skinMat = new THREE.MeshStandardMaterial({
+      roughness: 0.3, // 粗糙度
+      metalness: 0.8, // 金屬感
+      transparent: true, // 透明與否
+      opacity: 0.9, // 透明度
+      side: THREE.DoubleSide, // 雙面材質
+      map: skinMap // 皮膚貼圖
+    })
+
+    // 準備頭部與臉的材質
+    const headMaterials = []
+    for (let i = 0; i < 6; i++) {
+      let map
+
+      if (i === 4) map = headMap
+      else map = skinMap
+
+      headMaterials.push(new THREE.MeshStandardMaterial({ map: map }))
+    }
+
+    // 頭
+    this.head = new THREE.Mesh(headGeo, headMaterials)
+    this.head.position.set(0, 6, 0)
+    this.head.rotation.y = 0.5 // 稍微的擺頭
+
+    // 身體
+    this.body = new THREE.Mesh(bodyGeo, skinMat)
+    this.body.position.set(0, 0, 0)
+
+    // 四隻腳
+    this.foot1 = new THREE.Mesh(footGeo, skinMat)
+    this.foot1.position.set(-1, -5.5, 2)
+    this.foot2 = this.foot1.clone() // 剩下三隻腳都複製第一隻的 Mesh
+    this.foot2.position.set(-1, -5.5, -2)
+    this.foot3 = this.foot1.clone()
+    this.foot3.position.set(1, -5.5, 2)
+    this.foot4 = this.foot1.clone()
+    this.foot4.position.set(1, -5.5, -2)
+
+    // 將四隻腳組合為一個 group
+    this.feet = new THREE.Group()
+    this.feet.add(this.foot1)
+    this.feet.add(this.foot2)
+    this.feet.add(this.foot3)
+    this.feet.add(this.foot4)
+
+    // 將頭、身體、腳組合為一個 group
+    this.creeper = new THREE.Group()
+    this.creeper.add(this.head)
+    this.creeper.add(this.body)
+    this.creeper.add(this.feet)
+  }
+}
+
+// 生成苦力怕並加到場景
+function createCreeper() {
+  creeperObj = new Creeper()
+  scene.add(creeperObj.creeper)
+}
+
+function initStats() {
+  const stats = new Stats()
+  stats.setMode(0)
+  document.getElementById('stats').appendChild(stats.domElement)
+  return stats
+}
+
+// 畫面初始化
+function init() {
+  scene = new THREE.Scene()
+
+  // 相機設定
+  camera = new THREE.PerspectiveCamera(
+    60,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
+  )
+  camera.position.set(30, 30, 30)
+  camera.lookAt(scene.position)
+
+  // 建立 OrbitControls
+  cameraControl = new THREE.OrbitControls(camera)
+  cameraControl.enableDamping = true // 啟用阻尼效果
+  cameraControl.dampingFactor = 0.25 // 阻尼系數
+  // cameraControl.autoRotate = true // 啟用自動旋轉
+
+  let axes = new THREE.AxesHelper(20)
+  // scene.add(axes)
+
+  stats = initStats()
+
+  // 渲染器設定
+  renderer = new THREE.WebGLRenderer()
+  renderer.setSize(window.innerWidth, window.innerHeight)
+
+  // 簡單的地板
+  const planeGeometry = new THREE.PlaneGeometry(60, 60)
+  const planeMaterial = new THREE.MeshLambertMaterial({ color: 0xffffff })
+  let plane = new THREE.Mesh(planeGeometry, planeMaterial)
+  plane.rotation.x = -0.5 * Math.PI
+  plane.position.set(0, -7, 0)
+  scene.add(plane)
+
+  // 產生苦力怕物體
+  createCreeper()
+
+  let ambientLight = new THREE.AmbientLight(0x404040) // soft white light
+  scene.add(ambientLight)
+  // 簡單的 spotlight 照亮物體
+  let spotLight = new THREE.SpotLight(0xffffff, 5, 100)
+  spotLight.position.set(-10, 20, 20)
+  scene.add(spotLight)
+  // let spotHelper = new THREE.SpotLightHelper(spotLight)
+  // scene.add(spotHelper)
+
+  // 將渲染出來的畫面放到網頁上的 DOM
+  document.body.appendChild(renderer.domElement)
+}
+
+function render() {
+  stats.update()
+  requestAnimationFrame(render)
+  cameraControl.update()
+  renderer.render(scene, camera)
+}
+
+window.addEventListener('resize', function() {
+  camera.aspect = window.innerWidth / window.innerHeight
+  camera.updateProjectionMatrix()
+  renderer.setSize(window.innerWidth, window.innerHeight)
+})
+
+init()
+render()
